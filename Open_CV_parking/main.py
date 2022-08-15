30cd54301803cf4230901df0ebcef43db5f05741
mullerdylan <56490868+mullerdylan@users.noreply.github.com>
2022-08-15 10:17:02 -0700

    Implemented duration range to run app
    Done. need to deploy, but all features implemented

--- Open_CV_parking/main.py
+++ Open_CV_parking/main.py
@@ -1,40 +1,56 @@
+from asyncio.log import logger
 import cv2
 import pickle
 import numpy as np 
 import datetime
+import pandas as pd
+import os
 
 width,height = 175,100
 log = []
+parking_position_list = []
+timestamp_list = []
+availability_list = []
+date = datetime.datetime.now().date()
+
+def frame_count(start,end,refresh_rate_min):
+    delta = (datetime.datetime.strptime(end,'%H:%M:%S') - datetime.datetime.strptime(start,'%H:%M:%S')).total_seconds()
+    frames = round(delta/(refresh_rate_min*60))
+    return frames
+
 def checkspace(imgpro): 
     spacecounter = 0
     timestamp = datetime.datetime.now()
+    
     for pos in posList:
         x,y = pos
-
         imgcrop = imgpro[y:y+height, x:x+width]
         cv2.imshow(str(pos), imgcrop)
         count = cv2.countNonZero(imgcrop)
         cv2.putText(img,str(count),(x,y+height-10), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1,(255,255,255))
         if count <1000:
-            available = 0
+            availability = 0
             color = (0,255,0)
-            logger = F'pos: {posList.index(pos)}, time: {timestamp} , available: {available}'
-            print(logger)
-            log.append(logger)
             spacecounter +=1
         else: 
-            available = 1
-            logger = F'pos: {posList.index(pos)},time: {timestamp} , available: {available}'
-            print(logger)
-            log.append(logger)
-            color = (0,0,255)
+            availability = 1
+            color = (0,0,255) 
+
+        parking_position_list.append(posList.index(pos))
+        timestamp_list.append(timestamp)
+        availability_list.append(availability)
+
         cv2.rectangle(img,pos,(pos[0]+width,pos[1]+height),color,2)
     cv2.putText(img,f"Spaces Available: {spacecounter}",(100,100), cv2.FONT_HERSHEY_PLAIN, 2,(255,255,255))
 with open('carparkpos','rb') as f:
         posList = pickle.load(f)
 
 vid = cv2.VideoCapture(1)
-while True:
+# Function format: (start time, End time, minutes between frame )
+inerval_min = .1
+max_frame = frame_count('7:00:00','7:01:00',inerval_min)
+
+while len(availability_list)<max_frame*len(posList):
     ret, frame = vid.read()
     img = frame
     imggray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -46,13 +62,16 @@ while True:
     kernel  = np.ones((3,3), np.uint8)
     imagedial = cv2.dilate(imgmed, kernel,iterations = 1)
     checkspace(imagedial)
-
-    # for pos in posList:
-    #     cv2.rectangle(img,pos,(pos[0]+width,pos[1]+height),(255,0,255),2)
-
+    
     cv2.imshow("image",img)
-    # cv2.imshow("imageb",imgblur)
-    # cv2.imshow("imag",imgthreshold)
-    # cv2.imshow("im",imgmed)
-    # cv2.imshow("final", imagedial)
-    cv2.waitKey(100)
+
+    if cv2.waitKey(round(inerval_min*60*1000)) & 0xFF == ord('q'):
+        break
+# Logic for storing log after program finishes running 
+dict = {'pos' : parking_position_list,'timestamps' : timestamp_list , 'availabilty' :availability_list }
+df = pd.DataFrame(dict) 
+if os.path.exists(F'Logs\log_{date}.csv') ==True:
+    df.to_csv(F"Logs\log_{date}_alt.csv", index=False)
+else: 
+    df.to_csv(F"Logs\log_{date}.csv",index=False)
+print(df)
--- Open_CV_parking/testymctest.py
+++ Open_CV_parking/testymctest.py
@@ -1,20 +1,28 @@
-# import os
-# import datetime
-# date = datetime.datetime.now().date()
-# print(os.path.exists(F'log_{date}.csv'))
-
+import os
 import datetime
-def frame_count(start,end,refresh_rate_min):
-    delta = (datetime.datetime.strptime(end,'%H:%M:%S') - datetime.datetime.strptime(start,'%H:%M:%S')).total_seconds()
-    frames = round(delta/(refresh_rate_min*60))
-    return frames
+date = datetime.datetime.now().date()
+# print(os.path.exists(F'Logs/log_{date}.csv'))
+# if os.path.exists(F'log_{date}.csv') ==True:
+#     print('true')
+# else: 
+#     print('aaaaaaah')
+# print(os.path.exists(F'log_{date}.csv'))
+# import datetime
+# def frame_count(start,end,refresh_rate_min):
+#     delta = (datetime.datetime.strptime(end,'%H:%M:%S') - datetime.datetime.strptime(start,'%H:%M:%S')).total_seconds()
+#     frames = round(delta/(refresh_rate_min*60))
+#     return frames
 
-a = datetime.datetime.strptime('7:00:38','%H:%M:%S')
-b = datetime.datetime.strptime('18:35:38','%H:%M:%S')
-delta = (b-a).total_seconds()
+# a = datetime.datetime.strptime('7:00:38','%H:%M:%S')
+# b = datetime.datetime.strptime('18:35:38','%H:%M:%S')
+# delta = (b-a).total_seconds()
 
-refresh_rate_min = 5
+# refresh_rate_min = 5
 
-frames = round(delta/(refresh_rate_min*60))
-print(frames)
-print(frame_count('7:00:38','18:35:38',5))
+# frames = round(delta/(refresh_rate_min*60))
+# print(frames)
+# print(frame_count('7:00:38','18:35:38',5))
+
+from event_logger import frame_count
+inerval_min = 5
+print(frame_count('7:00:00','7:01:00',inerval_min))
